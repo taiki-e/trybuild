3f3fd5186e1612073bb1faf1173e9119db862264
Taiki Endo <te316e89@gmail.com>
2024-09-15 08:17:54 +0900

    Revert "Combine all rustflags into cargo --config arg"
    This reverts commit 1b8d4bdf0718bfe4719039ad2d3d53c9f5ccdab5.

--- src/rustflags.rs
+++ src/rustflags.rs
@@ -1,12 +1,27 @@
+use std::env;
+use std::ffi::OsString;
+
+const RUSTFLAGS: &str = "RUSTFLAGS";
 const IGNORED_LINTS: &[&str] = &["dead_code"];
 
-pub(crate) fn toml() -> toml::Value {
-    let mut rustflags = vec!["--cfg", "trybuild", "--verbose"];
+pub(crate) fn make_vec() -> Vec<&'static str> {
+    let mut rustflags = vec!["--cfg", "trybuild"];
 
     for &lint in IGNORED_LINTS {
         rustflags.push("-A");
         rustflags.push(lint);
     }
 
-    toml::Value::try_from(rustflags).unwrap()
+    rustflags
+}
+
+pub(crate) fn envs() -> impl IntoIterator<Item = (&'static str, OsString)> {
+    let mut rustflags = env::var_os(RUSTFLAGS)?;
+
+    for flag in make_vec() {
+        rustflags.push(" ");
+        rustflags.push(flag);
+    }
+
+    Some((RUSTFLAGS, rustflags))
 }
