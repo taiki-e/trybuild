88220469ce30c7d0ce8dd2f1af9f11df4276da86
Taiki Endo <te316e89@gmail.com>
2025-02-01 15:48:08 +0900

    Handle CARGO_ENCODED_RUSTFLAGS

--- src/rustflags.rs
+++ src/rustflags.rs
@@ -1,6 +1,7 @@
 use std::env;
 use std::ffi::OsString;
 
+const CARGO_ENCODED_RUSTFLAGS: &str = "CARGO_ENCODED_RUSTFLAGS";
 const RUSTFLAGS: &str = "RUSTFLAGS";
 const IGNORED_LINTS: &[&str] = &["dead_code"];
 
@@ -16,12 +17,28 @@ pub(crate) fn make_vec() -> Vec<&'static str> {
 }
 
 pub(crate) fn envs() -> impl IntoIterator<Item = (&'static str, OsString)> {
-    let mut rustflags = env::var_os(RUSTFLAGS)?;
+    // The precedence of rustflags is:
+    // 1. CARGO_ENCODED_RUSTFLAGS
+    // 2. RUSTFLAGS
+    // 3. target.<triple>.rustflags (CARGO_TARGET_<triple>_RUSTFLAGS) and target.<cfg>.rustflags
+    // 4. build.rustflags (CARGO_BUILD_RUSTFLAGS)
+    // Refs: https://doc.rust-lang.org/nightly/cargo/reference/config.html#buildrustflags
+    // For now, skip 3 and 4 because 3 is complex and handling 4 without 3 incorrectly overwrite rustflags.
+    // TODO: Consider using cargo-config2 crate that implements it.
+    let (key, mut val, separator) = match env::var_os(CARGO_ENCODED_RUSTFLAGS) {
+        Some(val) => (CARGO_ENCODED_RUSTFLAGS, val, "\x1f"),
+        None => match env::var_os(RUSTFLAGS) {
+            Some(val) => (RUSTFLAGS, val, " "),
+            None => return None,
+        },
+    };
 
     for flag in make_vec() {
-        rustflags.push(" ");
-        rustflags.push(flag);
+        if !val.is_empty() {
+            val.push(separator);
+        }
+        val.push(flag);
     }
 
-    Some((RUSTFLAGS, rustflags))
+    Some((key, val))
 }
